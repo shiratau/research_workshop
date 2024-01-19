bb26cdf0c464791b1db5593ed665d7751b70f636
shiratau <shiraevron@mail.tau.ac.il>
2024-01-19 15:44:20 +0200

    primitive first stage works

--- tests/test_predictions_multi.py
+++ tests/test_predictions_multi.py
@@ -39,28 +39,58 @@ def test_predict_multi_numbers_from_seq():
 def test_predict_sd_normal():
     raw_df = pd.read_csv("multi_dataset7.csv")
     print(raw_df.head())
-    samples_input = [np.array(ast.literal_eval(sample), dtype=float) for sample in raw_df["sample"]]
-    sd_output = [_ for _ in raw_df["sd"]]
-
+    samples_input, sd_output = _get_data_from_df(raw_df)
     df = pd.DataFrame(
         {
             "input": pd.Series(samples_input),
-            "output": sd_output
+            "output": pd.Series(sd_output)
         }
     )
 
     predictions, y_test = predict_multi_sd.run(df)
+    print(predictions)
+    print(y_test)
 
     suc = 0
     for i in range(len(predictions)):
-        prd = predictions.flat[i]
-        exp = y_test.flat[i]
-        cmp = _compare_result_in_allowed_range(prd, exp)
-        print(f'prediction: {prd}, expected: {exp}, comparison: {cmp}')
-        if cmp:
-            suc += 1
+        sd_prd_set = predictions[i]
+        sd_exp_set = y_test[i]
+        for j in range(len(sd_prd_set)):
+            prd = sd_prd_set[j]
+            exp = sd_exp_set[j]
+            cmp = _compare_result_in_allowed_range(prd, exp)
+            print(f'set: {i}, prediction: {prd}, expected: {exp}, comparison: {cmp}')
+            if cmp:
+                suc += 1
+
     print(f"num of success: {suc}")
-    print(f"num of fails: {len(predictions) - suc}")
+    print(f"num of fails: {len(predictions)*3 - suc}")
+
+
+def _get_data_from_df(raw_df):
+    num_of_agents = 10  # todo: read from dataset metadata
+    samples_input = []
+    sd_output = []
+
+    for agent_id in range(num_of_agents):
+        agents_rows = raw_df.loc[raw_df['Agent'] == agent_id]
+        samples_bulk_feed = []
+        sd_series = []
+
+        for sample in agents_rows["sample"]:
+            samples_bulk_feed += ast.literal_eval(sample)
+
+        samples_input.append(pd.Series(np.array(samples_bulk_feed, dtype=float)))
+
+        for sd in agents_rows["sd"]:
+            if sd not in sd_series:
+                sd_series.append(sd)
+
+        sd_output.append(pd.Series(np.array(sd_series, dtype=float)))
+
+    print(samples_input)
+    print(sd_output)
+    return samples_input, sd_output
 
 
 def _compare_result_in_allowed_range(prediction, y_test_value) -> bool:
