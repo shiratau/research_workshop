88771bf5d1fd800dcb712afb46397fc2dddbfb56
shiratau <shiraevron@mail.tau.ac.il>
2024-07-02 16:50:37 +0300

    scripts cleaups

--- feature/create_datasets_with_feature.py
+++ feature/create_datasets_with_feature.py
@@ -1,10 +1,5 @@
-from datetime import datetime
-from random import randrange, uniform, randint
-
 from lib import *
 
-FILE_ID = 'feature_dataset6'
-FILE_PATH_TEMPLATE = "./datasets/{}.csv"
 NUMBER_OF_AGENTS = 100
 NUMBER_OF_DIST_PER_AGENT = 3
 MAX_SIZE_OF_SAMPLE = 210
@@ -15,7 +10,10 @@ SHOULD_ROUND = True
 N_DIGIT = 5
 
 
-def run_script():
+def run_script(file_id='feature_dataset'):
+    file_id = f'{file_id}_{int(datetime.now().timestamp())}'
+    print(f'file id: {file_id}')
+
     df = pd.DataFrame(columns=['Agent', 'sample_size', 'sample', 'timestamps', 'mu', 'sd'])
 
     for agent_id in range(NUMBER_OF_AGENTS):
@@ -29,10 +27,10 @@ def run_script():
             curr_timestamps = _create_timestamps_seq(len(curr_sample), dist_num)
             row = [agent_id, sample_size, curr_sample, curr_timestamps, curr_mu, curr_sd]
             df.loc[len(df.index)] = row
-        # print(f"done Agent {agent_id}")
 
-    _write_csv(df)
-    _write_metadata()
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    df.to_csv(f'./datasets/{file_id}.csv')
+    _write_metadata(file_id)
 
 
 def _create_timestamps_seq(size, dist_num):
@@ -44,11 +42,10 @@ def _create_timestamps_seq(size, dist_num):
 
 
 def _pos_normal(mu, sd, sample_size):
-    # make sure all numbers are in (0.02, 2.0) range, and not negative.
-    # todo: fix negative possibility, maybe shift values by shifting mu
+    # this should make sure all values are positive, but manual check still needed
     sample = np.random.normal(mu, sd, sample_size)
     for _ in range(10):
-        if not all(0.02 < x < 2.0 for x in sample):
+        if not all(x > 0 for x in sample):
             sample = np.random.normal(mu, sd, sample_size)
     return [_round_if_configured(x) for x in sample]
 
@@ -85,24 +82,9 @@ def _split_total_samples_num_to_samples_groups(n):
     return result
 
 
-def _save_sample_in_histogram(sample, sigma, mu):
-    count, bins, ignored = plt.hist(sample, 30, density=True)
-    plt.plot(bins, 1 / (sigma * np.sqrt(2 * np.pi)) *
-             np.exp(- (bins - mu) ** 2 / (2 * sigma ** 2)),
-             linewidth=2, color='r')
-    plt.savefig(f'figs/his({mu},{sigma}.png', bbox_inches='tight')
-
-
-def _write_csv(df):
-    if not os.path.exists(FILE_PATH_TEMPLATE.format(FILE_ID)):
-        df.to_csv(FILE_PATH_TEMPLATE.format(FILE_ID))
-    else:
-        df.to_csv(FILE_PATH_TEMPLATE.format(f'{FILE_ID}_new'))
-
-
-def _write_metadata():
+def _write_metadata(file_id):
     metadata = [
-        f'FILE_ID: {FILE_ID}',
+        f'FILE_ID: {file_id}',
         f'NUMBER_OF_AGENTS: {NUMBER_OF_AGENTS}',
         f'NUMBER_OF_DIST_PER_AGENT: {NUMBER_OF_DIST_PER_AGENT}',
         f'MAX_SIZE_OF_SAMPLE: {MAX_SIZE_OF_SAMPLE}',
@@ -112,12 +94,15 @@ def _write_metadata():
         f'SHOULD_ROUND: {SHOULD_ROUND}',
         f'N_DIGIT: {N_DIGIT}',
     ]
-    with open(f'./datasets/metadata/{FILE_ID}.txt', 'w') as f:
+    with open(f'./datasets/metadata/{file_id}.txt', 'w') as f:
         for line in metadata:
             f.write(line)
             f.write('\n')
 
 
 if __name__ == '__main__':
-    run_script()
+    if len(sys.argv) > 1:
+        run_script(file_id=sys.argv[1])
+    else:
+        run_script()
     print("done.")
