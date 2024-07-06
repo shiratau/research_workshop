59453ae88532a18eff0a0a6bb50d7a042f680782
shiratau <shiraevron@mail.tau.ac.il>
2024-07-06 17:35:38 +0300

    passing file id to test from command line

--- lib/__init__.py
+++ lib/__init__.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 import pandas as pd
-import pytest
 from random import randrange, uniform, randint
 import sys
 import tensorflow as tf
